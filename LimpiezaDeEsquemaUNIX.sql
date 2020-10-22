28f5c4e8c0e5e164390e0441026ac8e8bebf8b4c
Leonel Bazan <leo.bazan.x@gmail.com>
2020-10-22 17:50:09 -0300

    Creo LimpiezaDeEsquemaUnix.sql
    Borra todo el esquema normalizado

---
+++ LimpiezaDeEsquemaUNIX.sql
@@ -0,0 +1,58 @@
+--------------------------------------------------------------------------
+----------------     Limpieza de Esquema UNIX            -----------------
+
+
+-- Definicion de Procedures
+CREATE PROCEDURE EliminarFKs
+AS
+    DECLARE @SQL as varchar(2000)
+    DECLARE @Schema as varchar(200)
+    DECLARE @Table as varchar(200)
+    DECLARE @Constraint as varchar(200)
+
+    DECLARE FKCursor CURSOR READ_ONLY FOR
+    SELECT Table_Schema,Table_Name, Constraint_Name
+    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+    WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
+
+    OPEN FKCursor
+    FETCH NEXT FROM FKCursor INTO @Schema,@Table,@Constraint
+    WHILE (@@fetch_status <> -1)
+    BEGIN
+    IF (@@fetch_status <> -2)
+    BEGIN
+    EXEC ('ALTER TABLE ' + @Schema + '.[' + @Table+ '] DROP CONSTRAINT [' + @Constraint + ']')
+    END
+    FETCH NEXT FROM FKCursor INTO @Schema,@Table,@Constraint
+    END
+
+    CLOSE FKCursor
+    DEALLOCATE FKCursor;
+
+
+GO;
+
+CREATE PROCEDURE EliminarEsquemaUnix
+AS
+	DROP TABLE UNIX.Automovil
+	DROP TABLE UNIX.Autoparte
+	DROP TABLE UNIX.Cliente
+	DROP TABLE UNIX.Compra
+	DROP TABLE UNIX.CompraPorAutoparte
+	DROP TABLE UNIX.CompraAutomovil
+	DROP TABLE UNIX.ItemAutomovil
+	DROP TABLE UNIX.ItemAutoparte
+	DROP TABLE UNIX.Fabricante
+	DROP TABLE UNIX.Factura
+	DROP TABLE UNIX.Modelo
+	DROP TABLE UNIX.Sucursal
+	DROP TABLE UNIX.TipoAuto
+	DROP TABLE UNIX.TipoCaja
+	DROP TABLE UNIX.TipoMotor
+	DROP TABLE UNIX.TipoTransmision
+	DROP SCHEMA UNIX;
+
+-- Ejecucion de Procedures
+
+EXEC EliminarFKs;
+EXEC EliminarEsquemaUnix;
