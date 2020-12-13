f357021472fa2c3733cece6622774cc6a8d0b896
Leonel Bazan <leo.bazan.x@gmail.com>
2020-12-13 16:29:27 -0300

    Agrego Querys de BI para testeo

---
+++ querys_utilizadas_BI.sql
@@ -0,0 +1,13 @@
+SELECT * FROM UNIX.BI_HechoModelo
+SELECT * FROM UNIX.BI_HechoAutoparteCompra
+SELECT * FROM UNIX.BI_HechoAutoparteVenta
+
+SELECT * FROM UNIX.BI_Vista_Automoviles_Cantidad_Comprados_Vendidos
+SELECT * FROM UNIX.BI_Vista_Automoviles_Ganancias
+SELECT * FROM UNIX.BI_Vista_Automoviles_Stock
+SELECT * FROM UNIX.BI_Vista_Automoviles_Precio_Promedio
+
+SELECT * FROM UNIX.BI_Vista_Autopartes_Precio_Promedio
+SELECT * FROM UNIX.BI_Vista_Autopartes_Ganancias
+SELECT * FROM UNIX.BI_Vista_Autopartes_Stock_mensual_sucursal
+SELECT * FROM UNIX.BI_Vista_Autopartes_Stock_Max_Anual
