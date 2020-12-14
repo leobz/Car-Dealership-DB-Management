7ccabc4e8a63d60bc015cbdaeb1c61c41577cdae
Leonel Bazan <leo.bazan.x@gmail.com>
2020-12-13 21:01:36 -0300

    General: Limpieza definitiva de Esquema UNIX

--- LimpiezaDeEsquemaUNIX.sql
+++ LimpiezaDeEsquemaUNIX.sql
@@ -4,12 +4,29 @@
 USE [GD2C2020]
 GO
 
-IF(OBJECT_ID('EliminarFKs') IS NOT NULL)
-	DROP PROCEDURE EliminarFKs
+/* Drop all views */
+DECLARE @name VARCHAR(128)
+DECLARE @SQL VARCHAR(254)
+
+SELECT @name = (SELECT TOP 1 v.name FROM sys.views as v WHERE OBJECT_SCHEMA_NAME(v.object_id) = 'UNIX')
+
+WHILE @name IS NOT NULL
+BEGIN
+    SELECT @SQL = 'DROP VIEW [UNIX].[' + RTRIM(@name) +']'
+    EXEC (@SQL)
+    PRINT 'Dropped View: ' + @name
+    SELECT @name = (SELECT TOP 1 v.name FROM sys.views as v WHERE OBJECT_SCHEMA_NAME(v.object_id) = 'UNIX' AND v.name > @name ORDER BY v.name)
+END
 GO
 
-IF(OBJECT_ID('EliminarEsquemaUnix') IS NOT NULL)
-	DROP PROCEDURE EliminarEsquemaUnix
+/* Drop function */
+IF(OBJECT_ID('CalcularRangoEdad') IS NOT NULL)
+	DROP FUNCTION CalcularRangoEdad
+GO
+
+/* Drop all Foreing Keys */
+IF(OBJECT_ID('EliminarFKs') IS NOT NULL)
+	DROP PROCEDURE EliminarFKs
 GO
 
 CREATE PROCEDURE EliminarFKs
@@ -18,12 +35,10 @@ DECLARE @SQL as varchar(2000)
 DECLARE @Schema as varchar(200)
 DECLARE @Table as varchar(200)
 DECLARE @Constraint as varchar(200)
-
 DECLARE FKCursor CURSOR READ_ONLY FOR
-        SELECT Table_Schema, Table_Name, Constraint_Name
-FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
-
+  SELECT Table_Schema, Table_Name, Constraint_Name
+  FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+  WHERE CONSTRAINT_TYPE = 'FOREIGN KEY'
 OPEN FKCursor
 FETCH NEXT FROM FKCursor INTO @Schema,@Table,@Constraint
 WHILE (@@fetch_status <> -1)
@@ -34,33 +49,37 @@ WHILE (@@fetch_status <> -1)
     END
     FETCH NEXT FROM FKCursor INTO @Schema,@Table,@Constraint
 END
-
 CLOSE FKCursor
 DEALLOCATE FKCursor
 GO
 
-CREATE PROCEDURE EliminarEsquemaUnix
-AS
-DROP TABLE UNIX.Automovil
-DROP TABLE UNIX.Autoparte
-DROP TABLE UNIX.Cliente
-DROP TABLE UNIX.Compra
-DROP TABLE UNIX.CompraPorAutoparte
-DROP TABLE UNIX.CompraAutomovil
-DROP TABLE UNIX.ItemAutomovil
-DROP TABLE UNIX.ItemAutoparte
-DROP TABLE UNIX.Fabricante
-DROP TABLE UNIX.Factura
-DROP TABLE UNIX.Modelo
-DROP TABLE UNIX.Sucursal
-DROP TABLE UNIX.TipoAuto
-DROP TABLE UNIX.TipoCaja
-DROP TABLE UNIX.TipoMotor
-DROP TABLE UNIX.TipoTransmision
-DROP SCHEMA UNIX
+/* Drop all tables */
+IF(OBJECT_ID('EliminarTablas') IS NOT NULL)
+	DROP PROCEDURE EliminarTablas
 GO
 
--- Ejecucion de Procedures
+CREATE PROCEDURE EliminarTablas
+AS
+DECLARE @Table as varchar(200)
+DECLARE TableCursor CURSOR READ_ONLY FOR
+SELECT t.name 
+FROM sys.tables t
+WHERE schema_name(t.schema_id) = 'UNIX';
+OPEN TableCursor
+FETCH NEXT FROM TableCursor INTO @Table
+WHILE (@@fetch_status <> -1)
+        BEGIN
+    IF (@@fetch_status <> -2)
+        BEGIN
+        EXEC ('DROP TABLE [UNIX]. ' + @Table)
+    END
+    FETCH NEXT FROM TableCursor INTO @Table
+END
+CLOSE TableCursor
+DEALLOCATE TableCursor
+GO
 
 EXEC EliminarFKs;
-EXEC EliminarEsquemaUnix;
+EXEC EliminarTablas
+
+DROP SCHEMA IF EXISTS UNIX; 
